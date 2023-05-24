c2a9309f416cd27643202f1f769bf2dda920fec3
Julia Tomasson <jjuliatomasson@gmail.com>
2023-05-24 18:55:38 +0200

    categories

--- frontend/src/components/DesignLanguageContainer.tsx
+++ frontend/src/components/DesignLanguageContainer.tsx
@@ -1,60 +1,39 @@
-import { FunctionComponent, useMemo } from "react";
-import CSS, { Property } from "csstype";
+import { FunctionComponent } from "react";
 
-type DesignLanguageContainerType = {
-  designLanguageText?: string;
-  designPracticesText?: string;
-  chairText?: string;
-
-  /** Style props */
-  propJustifyContent?: Property.JustifyContent;
-  propWidth?: Property.Width;
-};
-
-const DesignLanguageContainer: FunctionComponent<
-  DesignLanguageContainerType
-> = ({
-  designLanguageText,
-  designPracticesText,
-  chairText,
-  propJustifyContent,
-  propWidth,
-}) => {
-  const frameDivStyle: CSS.Properties = useMemo(() => {
-    return {
-      justifyContent: propJustifyContent,
-    };
-  }, [propJustifyContent]);
-
-  const lexendsDesignLanguageStyle: CSS.Properties = useMemo(() => {
-    return {
-      width: propWidth,
-    };
-  }, [propWidth]);
-
+const LexendContainer: FunctionComponent = () => {
   return (
+    <div>
+
+
+    <div className="flex flex-col lg:flex-row lg:bg-white w-fit px-[15px] lg:px-0 items-center lg:w-auto">
+    <img
+        className="relative lg:w-3/5 w-full  object-cover "
+        alt=""
+        src="/rectangle-116@2x.png"
+      />
     <div
-      className="flex flex-col items-start justify-center gap-[64px] text-left text-29xl text-black font-body-b1"
-      style={frameDivStyle}
+      className="flex flex-col  justify-center text-black px-[12px]"
     >
-      <div className="flex flex-col items-start justify-start gap-[32px]">
+      <div className="flex flex-col items-start justify-start">
         <div
-          className="relative tracking-[-0.07em] inline-block w-[388px]"
-          style={lexendsDesignLanguageStyle}
+          className="relative tracking-[-0.07em] leading-[120%] inline-block text-[24px] lg:text-[48px] lg:font-normal font-light"
+
         >
-          {designLanguageText}
+          The significance of choosing the right chair
         </div>
-        <div className="relative text-5xl leading-[120%] font-light inline-block w-[423px]">
-          {designPracticesText}
-        </div>
+        <p className="relative text-5xl leading-[120%] font-light text-[16px] lg:text-[24px] inline-block">
+        Chat or schedule a personal meeting with one of our experts on interior design to perfect your style.
+        </p>
       </div>
-      <button className="cursor-pointer py-4 px-16 bg-[transparent] rounded-31xl box-border h-[72px] flex flex-row items-center justify-center border-[4px] border-solid border-brown">
-        <div className="relative text-5xl tracking-[0.15em] font-medium font-clash-grotesk text-brown text-left">
-          {chairText}
-        </div>
+      <div className="flex justify-center items-center pt-[16px]">
+      <button className="cursor-pointer bg-[transparent] rounded-31xl h-[45.42px] w-[149px] lg:h-[72px] lg:w-[274px] border-[2.03px] lg:border-[4px] border-solid border-brown font-medium tracking-[0.1em] text-brown text-[16px] lg:text-[24px]">
+        Read more
       </button>
+      </div>
+    </div>
+    </div>
     </div>
   );
 };
 
-export default DesignLanguageContainer;
+export default LexendContainer;
