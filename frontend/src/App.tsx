927e7b0c37dfd15c83680e160bb1bc3055579b81
John Tönnäng <john_tonnang@hotmail.com>
2023-05-26 10:10:59 +0200

    Fixed navigation error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,48 @@
+// import { Routes, Route } from "react-router-dom"
+// import HomeView from "./routes/HomeView"
+// import CategoriesView from "./routes/CategoriesView"
+// import DetailPage from "./routes/DetailPage"
+// import { useEffect, useState } from "react"
+
+// interface Category {
+//   category_id: number
+//   name: string
+//   image: string
+// }
+
+// function App() {
+//   const [categories, setCategories] = useState<Category[]>([])
+
+//   useEffect(() => {
+//     fetchCategories()
+//   }, [])
+
+//   const fetchCategories = async () => {
+//     try {
+//       const response = await fetch("http://localhost:3000/categories")
+//       const data = await response.json()
+//       setCategories(data)
+//     } catch (error) {
+//       console.error("Error fetching categories:", error)
+//     }
+//   }
+
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<HomeView />} />
+//         <Route
+//           path={`/${category.name.toLowerCase()}`}
+//           element={<CategoriesView />}
+//         />
+//         <Route path="/detail" element={<DetailPage />} />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+
 import { Routes, Route } from "react-router-dom"
 import HomeView from "./routes/HomeView"
 import CategoriesView from "./routes/CategoriesView"
@@ -29,16 +74,17 @@ function App() {
 
   return (
     <>
-      {categories.map((category) => (
-        <Routes>
-          <Route path="/" element={<HomeView />} />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        {categories.map((category) => (
           <Route
+            key={category.category_id}
             path={`/${category.name.toLowerCase()}`}
             element={<CategoriesView />}
           />
-          <Route path="/detail" element={<DetailPage />} />
-        </Routes>
-      ))}
+        ))}
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
     </>
   )
 }
