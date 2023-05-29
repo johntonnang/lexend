9323ca2d9f6fde53beeeacefad3f3d7e7afd009b
Julia Tomasson <124245547+juliat0@users.noreply.github.com>
2023-05-29 13:47:18 +0200

    newsletterform

--- frontend/src/components/NewsletterForm.tsx
+++ frontend/src/components/NewsletterForm.tsx
@@ -2,37 +2,32 @@ import { FunctionComponent } from "react"
 
 const NewsletterForm: FunctionComponent = () => {
   return (
-<div className="relative lg:w-3/5 w-full">
+<div className="relative  w-full lg:flex lg:bg-white">
          <img
-          className="relative lg:w-3/5 w-full  object-cover "
+          className="relative lg:w-1/2 w-full  object-cover "
           alt=""
           src="/rectangle-104@2x.png"
         />
       <div
-        className="absolute top-[34px] left-[18px] bg-white bg-opacity-90 w-[355px] h-[214px] flex flex-col items-center justify-start text-left font-body-b1 text-29xl text-black-200"
+        className="absolute lg:relative top-[34px] lg:top-[0] bottom-[55px] left-[18px] right-[20px] bg-[#F2EBE3] lg:bg-white bg-opacity-90 flex flex-col items-center justify-center text-left font-body-b1 text-29xl text-black-200"
       >
         <div
-          className="w-full flex flex-col items-start justify-start"
-          style={{
-            fontSize: "24px",
-            lineHeight: "30px",
-            letterSpacing: "-0.07em",
-            color: "#000000",
-          }}
+          className="flex flex-col items-center justify-center mx-[10px]"
+
         >
-          <div className="text-center">Become a member of our family</div>
-          <div className="text-center text-[16px] font-light leading-[120%]">
+          <div className="text-center text-[24px] lg:text-[48px] tracking-[-0.07em] py-[22px] lg:py-[0] lg:pb-[32px] font-normail">Become a member of our family</div>
+          <div className="text-center text-[16px] lg:text-[24px] font-light leading-[120%] px-[35px] lg:px-[89px] pb-[18px] lg:pb-[87px]">
             Enjoy our newest pieces, exclusive events, limited offers and much more when joining our community.
           </div>
         </div>
-        <div className="flex items-center justify-between w-full px-8">
+        <div className="flex items-center justify-around px-8 lg:px[89px] gap-[19px] lg:gap-[32px]">
           <input
-            className="flex-grow mr-1 h-[18px] rounded-31xl border-2 border-solid border-grey pl-3  text-xl focus:outline-none tracking-[0.15em]"
+            className="h-[27px] w-[181px] lg:w-[404px] lg:h-[71px] font-light text-[8px] lg:text-[20px] rounded-31xl border-2 border-solid border-grey pl-[6px] lg:pl-[30px] focus:outline-none tracking-[0.15em] lg:tracking-[0.25em]"
             type="text"
             placeholder="Email adress"
           />
-          <button className="h-[10px] rounded-31xl border-4 border-solid border-black-100 bg-black-100 px-6 py-4">
-            <div className="text-s font-medium tracking-[0.15em] text-beige">
+          <button className="h-[27px] w-[96px] lg:h-[72px] lg:w-[266px] rounded-31xl border-4 border-solid border-black-100 bg-black-100">
+            <div className="text-s font-medium tracking-[0.25em] text-beige text-[8px] lg:text-[24px] ">
               Subscribe
             </div>
           </button>
