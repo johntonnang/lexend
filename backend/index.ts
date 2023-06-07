464fbb23e8eeaeb93495caf2fc99f7c1174585d6
John Tönnäng <john_tonnang@hotmail.com>
2023-06-07 15:47:00 +0200

    Update backend

--- backend/index.ts
+++ backend/index.ts
@@ -130,6 +130,8 @@ app.get("/product/:product", async (request, response) => {
   }
 })
 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000")
 })
