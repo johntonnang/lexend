53515f6cdf751cdfbcf0eaf477ea983aad036548
John Tönnäng <john_tonnang@hotmail.com>
2023-06-08 15:04:20 +0200

    Miss in fetch

--- frontend/src/routes/CategoriesView.tsx
+++ frontend/src/routes/CategoriesView.tsx
@@ -55,7 +55,7 @@ const CategoriesView: FunctionComponent = () => {
   useEffect(() => {
     const fetchProductsByCategory = async () => {
       try {
-        const response = await fetch(`products/${category}`)
+        const response = await fetch(`/products/${category}`)
         const data = await response.json()
         setProducts(data)
       } catch (error) {
